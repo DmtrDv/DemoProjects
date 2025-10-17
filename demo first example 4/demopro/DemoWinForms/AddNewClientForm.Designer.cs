3e68b963e7101fdb384e634ee38802828df41aa9
DmtrDv <dmitriybychkov75146@gmail.com>
2025-10-17 19:06:45 +0300

    added proj

---
+++ demo first example 4/demopro/DemoWinForms/AddNewClientForm.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace DemoProject
+{
+    partial class AddNewClientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name_textBox = new System.Windows.Forms.TextBox();
+            this.Description_textBox = new System.Windows.Forms.TextBox();
+            this.Phone_textBox = new System.Windows.Forms.TextBox();
+            this.Mail_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AddClient_button = new System.Windows.Forms.Button();
+            this.Cancel_button = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Name_textBox
+            // 
+            this.Name_textBox.Location = new System.Drawing.Point(76, 14);
+            this.Name_textBox.Name = "Name_textBox";
+            this.Name_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Name_textBox.TabIndex = 0;
+            // 
+            // Description_textBox
+            // 
+            this.Description_textBox.Location = new System.Drawing.Point(76, 41);
+            this.Description_textBox.Name = "Description_textBox";
+            this.Description_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Description_textBox.TabIndex = 1;
+            // 
+            // Phone_textBox
+            // 
+            this.Phone_textBox.Location = new System.Drawing.Point(75, 67);
+            this.Phone_textBox.Name = "Phone_textBox";
+            this.Phone_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Phone_textBox.TabIndex = 2;
+            // 
+            // Mail_textBox
+            // 
+            this.Mail_textBox.Location = new System.Drawing.Point(75, 93);
+            this.Mail_textBox.Name = "Mail_textBox";
+            this.Mail_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Mail_textBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Описание";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Телефон";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "E-mail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Аватар";
+            // 
+            // AddClient_button
+            // 
+            this.AddClient_button.Location = new System.Drawing.Point(18, 146);
+            this.AddClient_button.Name = "AddClient_button";
+            this.AddClient_button.Size = new System.Drawing.Size(75, 23);
+            this.AddClient_button.TabIndex = 10;
+            this.AddClient_button.Text = "ОК";
+            this.AddClient_button.UseVisualStyleBackColor = true;
+            // 
+            // Cancel_button
+            // 
+            this.Cancel_button.Location = new System.Drawing.Point(100, 145);
+            this.Cancel_button.Name = "Cancel_button";
+            this.Cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.Cancel_button.TabIndex = 11;
+            this.Cancel_button.Text = "Отмена";
+            this.Cancel_button.UseVisualStyleBackColor = true;
+            this.Cancel_button.Click += new System.EventHandler(this.Cancel_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AddNewClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(198, 181);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Cancel_button);
+            this.Controls.Add(this.AddClient_button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Mail_textBox);
+            this.Controls.Add(this.Phone_textBox);
+            this.Controls.Add(this.Description_textBox);
+            this.Controls.Add(this.Name_textBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddNewClientForm";
+            this.Text = "AddNewClientForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Name_textBox;
+        private System.Windows.Forms.TextBox Description_textBox;
+        private System.Windows.Forms.TextBox Phone_textBox;
+        private System.Windows.Forms.TextBox Mail_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button AddClient_button;
+        private System.Windows.Forms.Button Cancel_button;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ demo first example 4/demopro/DemoWinForms/ClientOrdersForm.cs
@@ -0,0 +1,90 @@
+﻿using DemoLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoProject
+{
+    public partial class ClientOrdersForm: Form
+    {
+        private User currentUser_ = null;
+       
+        public ClientOrdersForm(User user)
+        {
+            InitializeComponent();
+            currentUser_ = user;
+
+        }
+
+        public void SetOrder(Order order)
+        {
+            /// Д.З. Сделать масштабирование колонок таблицы по размеру окна
+            /// Добавить строку Итого
+            /// По цене: средняя цена, по стоимости - общая сумма, остальные - прочерки
+            OrdersTable.DataSource = null;
+            OrdersTable.DataSource = order.GetRecords();
+        }
+        private bool isLowRoleUser()
+        {
+            /// currentUser_ == null - это гость
+            return currentUser_.Role == UserRole.Manager;
+        }
+        public void AddAndRemoveOrders(Client obj)
+        {
+            if (obj == null)
+            {
+                return;
+            }
+
+            if (isLowRoleUser())
+            {
+                MessageBox.Show("У Вас не хватает прав, чтобы посмотреть заказы.\n" +
+                                "Обратитесь, пожалуйста, к администратору",
+                                "Сообщение",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+            }
+            else
+            {
+                ClientOrdersForm ordersForm = new ClientOrdersForm(currentUser_);
+                ordersForm.Text = "Заказы клиента " + obj.Name;
+                ordersForm.SetOrder(obj.order);
+                ordersForm.ShowDialog();
+            }
+        }
+        private void ClientOrdersForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            if (isLowRoleUser())
+            {
+                MessageBox.Show("У Вас не хватает прав, чтобы добавить заказы.\n" +
+                                "Обратитесь, пожалуйста, к администратору",
+                                "Сообщение",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            if (isLowRoleUser())
+            {
+                MessageBox.Show("У Вас не хватает прав, чтобы удалить заказы.\n" +
+                                "Обратитесь, пожалуйста, к администратору",
+                                "Сообщение",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
